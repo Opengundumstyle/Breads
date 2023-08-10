d22f546aec52086306f916e062ccf83c3b98fe83
Zhile Lin <zhlin04@gmail.com>
2023-08-09 17:54:26 -0700

    create bread car

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,43 @@
 
 //app/page
 
+import { fetchPosts } from "@/lib/actions/bread.actions"
+import { currentUser } from "@clerk/nextjs"
+import BreadCard from "@/components/cards/BreadCard"
+
 export default async function Home() {
 
-  // const results  = await fetchPost()
+  const results  = await fetchPosts(1,30)
+  const user = await currentUser()
+
+  console.log('result',results)
 
   return (
     <div>
        <h1 className="head-text text-left">Home</h1>
+       <section className="mt-9 flex flex-col gap-10">
+             {results.posts.length === 0?(
+                <p className="no-result">No Breads Found</p>
+             ):(
+                <>
+                  {
+                     results.posts.map((post)=>
+                       <BreadCard 
+                          key={post._id}
+                          id={post._id}
+                          currentUserId={user?.id || ""}
+                          parentId={post.parentId}
+                          content={post.text}
+                          author={post.author}
+                          community={post.community}
+                          createdAt={post.createdAt}
+                          comments = {post.children}
+                       />
+                       )
+                  }
+                </>
+             )}
+       </section>
     </div>
   )
 }
